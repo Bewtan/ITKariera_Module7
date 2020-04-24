5b80414bc47d737e8aa0272c6bb68a8dbfdf5772
Bewtan <g4e6o15r17g4i9@gmail.com>
2020-04-24 17:21:48 +0300

    Added Nullable data types to Data.Models.Book

--- Data/Models/Book.cs
+++ Data/Models/Book.cs
@@ -12,12 +12,13 @@ namespace Library.Data.Models
         public bool IsAvailable { get; set; }
         public DateTime DateOfPublishing { get; set; }
         public string Language { get; set;}
-        public DateTime DateOfBorrow { get; set; } //may not be needed
-        public DateTime DateOfReturn { get; set; } //clients shouldn't be able to borrow books if return period on other books has passed 
-        public int ClientId { get; set; }
+        public DateTime? DateOfBorrow { get; set; } //may not be needed
+        public DateTime? DateOfReturn { get; set; } //clients shouldn't be able to borrow books if return period on other books has passed 
+        public int? ClientId { get; set; }
         public virtual Client Client { get; set; }
-        public int PublisherId { get; set; }
+        public int? PublisherId { get; set; }
         public virtual Publisher Publisher { get; set; }
         public virtual ICollection<BooksGenres> BooksGenres { get; set; }
+
     }
 }
