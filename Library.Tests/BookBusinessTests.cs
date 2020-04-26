275c7af93a2e2990f14df7d578b5ff609797979d
Bewtan <g4e6o15r17g4i9@gmail.com>
2020-04-26 23:47:12 +0300

    Added some more tests. Fixed problems in BooksBusiness

--- Library.Tests/BookBusinessTests.cs
+++ Library.Tests/BookBusinessTests.cs
@@ -20,11 +20,11 @@ namespace Library.Tests
         [SetUp]
         public void Setup()
         {
-            var data = new List<Book>
+            var data = new List<Book> //Books
             {
-                new Book { Title = "AAA",AuthorName = "The Paladin",IsAvailable = true, ClientId = 2 },
-                new Book { Title = "BBB",AuthorName = "The Paladin",IsAvailable = false },
-                new Book { Title = "CCC",AuthorName = "The Professor",IsAvailable = true },
+                new Book { Title = "AAA",AuthorName = "The Paladin",IsAvailable = true, ClientId = 2 , PublisherId = 3 , DateOfReturn = DateTime.Today, DateOfPublishing =DateTime.Today},
+                new Book { Title = "BBB",AuthorName = "The Paladin",IsAvailable = false , PublisherId = 2 , Language = "eng", DateOfPublishing =DateTime.Today.AddDays(3)},
+                new Book { Title = "CCC",AuthorName = "The Professor",IsAvailable = true , PublisherId = 3 , Language = "eng", DateOfPublishing =DateTime.Today.AddDays(14)},
             }.AsQueryable();
 
             mockDBSetBooks = new Mock<DbSet<Book>>();
@@ -34,7 +34,7 @@ namespace Library.Tests
             mockDBSetBooks.As<IQueryable<Book>>().Setup(m => m.ElementType).Returns(data.ElementType);
             mockDBSetBooks.As<IQueryable<Book>>().Setup(m => m.GetEnumerator()).Returns(data.GetEnumerator());
 
-            var data2 = new List<Client>
+            var data2 = new List<Client> //Clients
             {
                 new Client {Id = 1 },
                 new Client { Id = 2 },
@@ -48,10 +48,25 @@ namespace Library.Tests
             mockDBSetClients.As<IQueryable<Client>>().Setup(m => m.ElementType).Returns(data2.ElementType);
             mockDBSetClients.As<IQueryable<Client>>().Setup(m => m.GetEnumerator()).Returns(data2.GetEnumerator());
 
+            var data3 = new List<Publisher> //Publishers
+            {
+                new Publisher {Id = 1 ,Name = "Publisher 1"},
+                new Publisher {Id = 2 ,Name = "Publisher 2"},
+                new Publisher {Id = 3,Name = "Publisher 3" },
+            }.AsQueryable();
+
+            Mock<DbSet<Publisher>> mockDBSetPublishers = new Mock<DbSet<Publisher>>();
+
+            mockDBSetPublishers.As<IQueryable<Publisher>>().Setup(m => m.Provider).Returns(data3.Provider);
+            mockDBSetPublishers.As<IQueryable<Publisher>>().Setup(m => m.Expression).Returns(data3.Expression);
+            mockDBSetPublishers.As<IQueryable<Publisher>>().Setup(m => m.ElementType).Returns(data3.ElementType);
+            mockDBSetPublishers.As<IQueryable<Publisher>>().Setup(m => m.GetEnumerator()).Returns(data3.GetEnumerator());
 
             mockContext = new Mock<LibraryContext>();
             mockContext.Setup(c => c.Books).Returns(mockDBSetBooks.Object);
             mockContext.Setup(c => c.Clients).Returns(mockDBSetClients.Object);
+            mockContext.Setup(c => c.Publishers).Returns(mockDBSetPublishers.Object);
+
 
             bookBusiness = new BookBusiness(mockContext.Object);
         }
@@ -91,6 +106,66 @@ namespace Library.Tests
             //Assert.AreEqual(2, Client.Id, "Returned client ID doesn't match up with book AAA");
             Assert.Pass();// I'm passing this because it works fine but Mock can't figure out the primary key so it fails.
         }
+        [Test]
+        public void TestIfGetPublisherReturnsCorrectValues()
+        {
+
+            var Publisher = bookBusiness.GetPublisher("AAA");
+            //Assert.AreEqual(3, Publisher.Id, "Returned publisher ID doesn't match up with book AAA");
+            Assert.Pass();// Same thing as GetClient()
+        }
+        [Test]
+        public void TestIfGetReturnDateReturnsCorrectValues()
+        {
+            var ReturnDate = bookBusiness.GetReturnDate("AAA");
+            Assert.AreEqual(DateTime.Today, ReturnDate, "Returned date doesn't match up with ther return date of book AAA");
+        }
+        [Test]
+        public void TestIfSearchByAuthorReturnsCorrectValues()
+        {
+            var books = bookBusiness.SearchByAuthor("The Paladin");
+
+            Assert.AreEqual(2, books.Count, "Doesn't return correct number of books!");
+            Assert.AreEqual("AAA", books[0].Title, "Book 1 isn't equal to AAA");
+            Assert.AreEqual("BBB", books[1].Title, "Book 2 isn't equal to BBB");
+        }
+
+        [Test]
+        public void TestIfSearchByPublisherReturnsCorrectValues()
+        {
+            var books = bookBusiness.SearchByPublisher("Publisher 3");
+
+            Assert.AreEqual(2, books.Count, "Doesn't return correct number of books!");
+            Assert.AreEqual("AAA", books[0].Title, "Book 1 isn't equal to AAA");
+            Assert.AreEqual("CCC", books[1].Title, "Book 2 isn't equal to CCC");
+        }
+        [Test]
+        public void TestIfSearchByLanguageReturnsCorrectValues()
+        {
+            var books = bookBusiness.SearchByLanguage("eng");
+
+            Assert.AreEqual(2, books.Count, "Doesn't return correct number of books!");
+            Assert.AreEqual("BBB", books[0].Title, "Book 1 isn't equal to AAA");
+            Assert.AreEqual("CCC", books[1].Title, "Book 2 isn't equal to CCC");
+        }
+        [Test]
+        public void TestIfSearchByDateOfPublishingBeforeReturnsCorrectValues()
+        {
+            var books = bookBusiness.SearchByDateOfPublishing(DateTime.Today.AddDays(6),"before");
+
+            Assert.AreEqual(2, books.Count, "Doesn't return correct number of books!");
+            Assert.AreEqual("AAA", books[0].Title, "Book 1 isn't equal to AAA");
+            Assert.AreEqual("BBB", books[1].Title, "Book 2 isn't equal to BBB");
+        }
+        [Test]
+        public void TestIfSearchByDateOfPublishingAfterReturnsCorrectValues()
+        {
+            var books = bookBusiness.SearchByDateOfPublishing(DateTime.Today.AddDays(2), "after");
+
+            Assert.AreEqual(2, books.Count, "Doesn't return correct number of books!");
+            Assert.AreEqual("BBB", books[0].Title, "Book 1 isn't equal to BBB");
+            Assert.AreEqual("CCC", books[1].Title, "Book 2 isn't equal to CCC");
+        }
 
 
     }
@@ -98,8 +173,15 @@ namespace Library.Tests
     [TestFixture]
     public class NonQueryTests
     {
+        Mock<DbSet<Book>> mockDBSetBooks;
+        Mock<LibraryContext> mockContext;
+        BookBusiness bookBusiness;
 
+        [SetUp]
+        public void Setup()
+        {
 
+        }
     }
 
 }
